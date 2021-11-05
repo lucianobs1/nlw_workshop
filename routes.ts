978a8964eb32e60881bc0fb32aae3d9509359da1
Luciano Brandao <lucianotetsu@gmail.com>
2021-11-05 18:37:12 -0300

    created user structure

--- routes.ts
+++ routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { SettingsController } from './src/controllers/SettingsController';
+import { UsersController } from './src/controllers/UserController';
 
 const routes = Router();
 
 routes.post('/settings', new SettingsController().create);
+routes.post('/users', new UsersController().create);
 
 export { routes };
