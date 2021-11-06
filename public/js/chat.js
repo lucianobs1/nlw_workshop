570319bda36d6e4ea4509f39e274b2f8afaa0991
Luciano Brandao <lucianotetsu@gmail.com>
2021-11-06 11:20:52 -0300

    added view in project

---
+++ public/js/chat.js
@@ -0,0 +1,7 @@
+// let socket_admin_id = null;
+// let emailUser = null;
+// let socket = null;
+
+document.querySelector('#start_chat').addEventListener('click', event => {
+  const socket = io();
+});
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,34 @@
 import express from 'express';
+import { createServer } from 'http';
+import path from 'path';
+import { Server, Socket } from 'socket.io';
 
 import { routes } from '../routes';
 import './database';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'html');
+
+app.get('/pages/client', (request, response) => {
+  return response.render('html/client.html');
+});
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on('connection', (socket: Socket) => {
+  console.log('Se conectou', socket.id);
+});
+
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
+http.listen(3333, () => {
   console.log('✨ Server is running ✨');
 });
