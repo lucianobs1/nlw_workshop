bdc0bf0baab49ddb7c0b06c8cfbe8b11046f9fcb
Luciano Brandao <lucianotetsu@gmail.com>
2021-11-09 21:18:18 -0300

    done communication sockets config  beetwen user/client

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
-// let socket_admin_id = null;
-// let emailUser = null;
-// let socket = null;
+let socket_admin_id = null;
+let emailUser = null;
+let socket = null;
 
 document.querySelector('#start_chat').addEventListener('click', event => {
   socket = io();
@@ -13,7 +13,6 @@ document.querySelector('#start_chat').addEventListener('click', event => {
 
   const email = document.getElementById('email').value;
   emailUser = email;
-
   const text = document.getElementById('txt_help').value;
 
   socket.on('connect', () => {
@@ -29,4 +28,60 @@ document.querySelector('#start_chat').addEventListener('click', event => {
       }
     });
   });
+  socket.on('client_list_all_messages', messages => {
+    console.log('messages', messages);
+    const template_client = document.getElementById(
+      'message-user-template',
+    ).innerHTML;
+    const template_admin = document.getElementById('admin-template').innerHTML;
+    messages.forEach(message => {
+      if (message.admin_id === null) {
+        const rendered = Mustache.render(template_client, {
+          message: message.text,
+          email,
+        });
+        document.getElementById('messages').innerHTML += rendered;
+      } else {
+        const rendered = Mustache.render(template_admin, {
+          message_admin: message.text,
+        });
+        document.getElementById('messages').innerHTML += rendered;
+      }
+    });
+  });
+  socket.on('admin_send_to_client', message => {
+    socket_admin_id = message.socket_id;
+
+    const template_admin = document.getElementById('admin-template').innerHTML;
+
+    const rendered = Mustache.render(template_admin, {
+      message_admin: message.text,
+    });
+
+    document.getElementById('messages').innerHTML += rendered;
+  });
 });
+
+document
+  .querySelector('#send_message_button')
+  .addEventListener('click', event => {
+    const text = document.getElementById('message_user');
+
+    const params = {
+      text: text.value,
+      socket_admin_id,
+    };
+
+    socket.emit('client_send_to_admin', params);
+
+    const template_client = document.getElementById(
+      'message-user-template',
+    ).innerHTML;
+
+    const rendered = Mustache.render(template_client, {
+      message: text.value,
+      email: emailUser,
+    });
+
+    document.getElementById('messages').innerHTML += rendered;
+  });
--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -11,7 +11,7 @@ interface IParams {
 io.on('connect', socket => {
   const connectionsService = new ConnectionsService();
   const usersService = new UsersService();
-  const messageService = new MessagesService();
+  const messagesService = new MessagesService();
 
   socket.on('client_first_access', async params => {
     const socket_id = socket.id;
@@ -31,6 +31,7 @@ io.on('connect', socket => {
       user_id = user.id;
     } else {
       user_id = userExists.id;
+
       const connection = await connectionsService.findByUserId(userExists.id);
 
       if (!connection) {
@@ -40,13 +41,34 @@ io.on('connect', socket => {
         });
       } else {
         connection.socket_id = socket_id;
+
         await connectionsService.create(connection);
       }
     }
 
-    await messageService.create({
+    await messagesService.create({
       text,
       user_id,
+    });
+
+    const allMessages = await messagesService.listByUser(user_id);
+
+    socket.emit('client_list_all_messages', allMessages);
+
+    const allUsers = await connectionsService.findAllWithoutAdmin();
+    io.emit('admin_list_all_users', allUsers);
+  });
+  socket.on('client_send_to_admin', async params => {
+    const { text, socket_admin_id } = params;
+    const socket_id = socket.id;
+    const { user_id } = await connectionsService.findBySocketID(socket.id);
+    const message = await messagesService.create({
+      text,
+      user_id,
+    });
+    io.to(socket_admin_id).emit('admin_receive_message', {
+      message,
+      socket_id,
     });
   });
 });
